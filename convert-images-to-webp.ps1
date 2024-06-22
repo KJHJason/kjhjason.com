403e89eeab2afcd5618f9ef60b7181a42d04a426
KJHJason <kuanjunhaojason@gmail.com>
2024-06-22 15:14:31 +0800

    add certs and awards and fix dockerfile

--- convert-images-to-webp.ps1
+++ convert-images-to-webp.ps1
@@ -1,6 +1,7 @@
 # Change the dir accordingly, could use Read-Host but getting issues with drive not existing
-$dir = "E:\Codes\Github Projects\kjhjason.com\res\misc"
+$dir = "E:\Codes\Github Projects\kjhjason.com\res\photos"
 $quality = 80
+$removeMetadata = $true
 
 # Convert all png and jpg images to webp without images in subdir
 $files = Get-ChildItem -Path $dir | Where-Object { $_.Extension -eq ".png" -or $_.Extension -eq ".gif" -or $_.Extension -eq ".jpg" -or $_.Extension -eq ".jpeg" }
@@ -18,6 +19,12 @@ foreach ($file in $files) {
     if ($file.Extension -eq ".jpg" -or $file.Extension -eq ".jpeg") {
         Write-Output "Converting $file to webp using ImageMagick to $replaced"
         magick $file.FullName -auto-orient -quality $quality $replaced # Using https://www.imagemagick.org/script/download.php#windows
+
+        if ($removeMetadata) {
+            # use exiftool to remove all metadata from the webp file
+            # Note: image orientation should still be preserved
+            exiftool -overwrite_original -all= $replaced
+        }
     } elseif ($file.Extension -eq ".gif") {
         gif2webp -q $quality -mt $file.FullName -o $replaced # https://developers.google.com/speed/webp/docs/gif2webp
     } else {
